d28997530da5e295a37defe4ce01d14fa1c924c4
tmannazy <e.anene@hotmail.com>
2022-04-16 19:15:30 +0100

    Refactor display of square & cube of numbers using escape character

--- chapterTwo/ExerciseTwoPrograms.java
+++ chapterTwo/ExerciseTwoPrograms.java
@@ -161,19 +161,41 @@ public void displayAdjacentNumbers() {
 
     public void displayTableOfSquaresAndCubes () {
         System.out.println("Question 2.31");
+        int numberToCalculate;
 
-        System.out.println("number square cube");
-        System.out.printf("%d      %d      %d%n", 0, 0 * 0, 0 * 0 * 0);
-        System.out.printf("%d      %d      %d%n", 1, 1 * 1, 1 * 1 * 1);
-        System.out.printf("%d      %d      %d%n", 2, 2 * 2, 2 * 2 * 2);
-        System.out.printf("%d      %d      %d%n", 3, 3 * 3, 3 * 3 * 3);
-        System.out.printf("%d     %d      %d%n", 4, 4 * 4, 4 * 4 * 4);
-        System.out.printf("%d     %d      %d%n", 5, 5 * 5, 5 * 5 * 5);
-        System.out.printf("%d     %d      %d%n", 6, 6 * 6, 6 * 6 * 6);
-        System.out.printf("%d     %d      %d%n", 7, 7 * 7, 7 * 7 * 7);
-        System.out.printf("%d     %d      %d%n", 8, 8 * 8, 8 * 8 * 8);
-        System.out.printf("%d     %d      %d%n", 9, 9 * 9, 9 * 9 * 9);
-        System.out.printf("%d    %d     %d%n", 10, 10 * 10, 10 * 10 * 10);
+        System.out.println("number" + "\t" + "square" + "\t" + "cube");
+        numberToCalculate = 0;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 1;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 2;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 3;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 4;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 5;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 6;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 7;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 8;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 9;
+        System.out.printf("%d\t%d\t%d%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
+
+        numberToCalculate = 10;
+        System.out.printf("%d\t%d\t%d%n%n", numberToCalculate, numberToCalculate * numberToCalculate, numberToCalculate * numberToCalculate * numberToCalculate);
     }
 
 
