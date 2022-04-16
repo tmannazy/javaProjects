01795cbadba3d821216020957dbffe4fbe68dd7b
tmannazy <e.anene@hotmail.com>
2022-04-16 10:11:47 +0100

    Rename text answers to Theoryanswers

--- chapterTwo/ExerciseTwoPrograms.java
+++ chapterTwo/ExerciseTwoPrograms.java
@@ -5,7 +5,6 @@ public class ExerciseTwoPrograms {
     int firstNumber;
     int secondNumber;
 
-// 2.8a
     public void displayEnterAnInteger(){
         System.out.println("Question 2.8a");
         System.out.print("Enter an integer: \n\n");
@@ -18,7 +17,6 @@ public class ExerciseTwoPrograms {
 // c
 // This program performs a sample payroll calculation.
 
-// 2.14
 public void displayAdjacentNumbers(){
         System.out.println("Question 2.14");
         System.out.println("1 2  3 4");
@@ -28,12 +26,9 @@ public void displayAdjacentNumbers(){
         System.out.print(" 4\n");
         System.out.printf("%s %s  %s %s%n%n","1","2","3","4");
 }
-
-// 2.15
     public void doSomeArithmeticWithUserInput(){
         System.out.println("Question 2.15");
         System.out.println("Please enter two whole numbers of your choice below");
-        System.out.print("Enter first number: ");
         firstNumber = userInput.nextInt();
         System.out.print("Enter second number: ");
         secondNumber = userInput.nextInt();
@@ -43,6 +38,21 @@ public void displayAdjacentNumbers(){
         System.out.printf("The square of your second number = %d%n",squareOfSecondNumber);
         System.out.printf("The sum of numbers when squared = %d%n", (squareOfFirstNumber + squareOfSecondNumber));
         System.out.printf("The difference of numbers when squared = %d%n%n",(squareOfFirstNumber - squareOfSecondNumber));
+    }
+
+    public void doSomeComparisonFromUserInputWithHundred(){
+        System.out.println("Question 2.16");
+        System.out.print("Please enter a whole number: ");
+        firstNumber = userInput.nextInt();
+        int squareOfFirstNumber = firstNumber * firstNumber;
+        if(firstNumber && squareOfFirstNumber > 100)
+            System.out.printf("Number entered %d and its square %d are greater than 100%n", firstNumber, squareOfFirstNumber);
+        if(firstNumber && squareOfFirstNumber == 100)
+            System.out.printf("Number entered %d and its square %d are equal to 100%n", firstNumber, squareOfFirstNumber);
+        if(firstNumber && squareOfFirstNumber != 100)
+            System.out.printf("Number entered %d and its square %d are not equal to 100%n", firstNumber, squareOfFirstNumber);
+        if(firstNumber && squareOfFirstNumber < 100)
+            System.out.printf("Number entered %d and its square %d are less than 100%n%n", firstNumber, squareOfFirstNumber);
     }
 
 //public void x%(){
