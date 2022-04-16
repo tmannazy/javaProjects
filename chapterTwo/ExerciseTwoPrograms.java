54fd3f19deb9c2b55bd9db358c6ca6793ce8ab9d
tmannazy <e.anene@hotmail.com>
2022-04-16 18:49:33 +0100

    Create a table of squares & cubes for numbers 0 - 10

--- chapterTwo/ExerciseTwoPrograms.java
+++ chapterTwo/ExerciseTwoPrograms.java
@@ -158,6 +158,26 @@ public void displayAdjacentNumbers() {
             System.out.printf("%d   ", (receivedNumber%10));
         }
     }
+
+    public void displayTableOfSquaresAndCubes () {
+        System.out.println("Question 2.31");
+
+        System.out.println("number square cube");
+        System.out.printf("%d      %d      %d%n", 0, 0 * 0, 0 * 0 * 0);
+        System.out.printf("%d      %d      %d%n", 1, 1 * 1, 1 * 1 * 1);
+        System.out.printf("%d      %d      %d%n", 2, 2 * 2, 2 * 2 * 2);
+        System.out.printf("%d      %d      %d%n", 3, 3 * 3, 3 * 3 * 3);
+        System.out.printf("%d     %d      %d%n", 4, 4 * 4, 4 * 4 * 4);
+        System.out.printf("%d     %d      %d%n", 5, 5 * 5, 5 * 5 * 5);
+        System.out.printf("%d     %d      %d%n", 6, 6 * 6, 6 * 6 * 6);
+        System.out.printf("%d     %d      %d%n", 7, 7 * 7, 7 * 7 * 7);
+        System.out.printf("%d     %d      %d%n", 8, 8 * 8, 8 * 8 * 8);
+        System.out.printf("%d     %d      %d%n", 9, 9 * 9, 9 * 9 * 9);
+        System.out.printf("%d    %d     %d%n", 10, 10 * 10, 10 * 10 * 10);
+    }
+
+
+
     public static void main(String... args) {
         ExerciseTwoPrograms supply = new ExerciseTwoPrograms();
 //        supply.displayEnterAnInteger();
@@ -172,11 +192,12 @@ public void displayAdjacentNumbers() {
 //        supply.circleTheoremCalculation();
 //        supply.getIntValueOfChar();
 //        supply.separateDigitsFromUser();
+        supply.displayTableOfSquaresAndCubes();
 
 
 
 
-//
+
 
     }
 }
