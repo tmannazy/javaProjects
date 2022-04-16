76b185acf948a49a7ac4a6a97e3b91fcb67371f3
tmannazy <e.anene@hotmail.com>
2022-04-16 08:09:35 +0100

    Create a file that houses all exercise programs & ignore irrelevant files

---
+++ chapterTwo/ExerciseTwoPrograms.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ExerciseTwoPrograms {
+    Scanner userInput = new Scanner(System.in);
+    int firstNumber;
+    int secondNumber;
+
+// 2.8a
+    public void displayEnterAnInteger(){
+        System.out.print("Enter an integer: ");
+    }
+
+// b
+//    int a, b, c;
+//    a = b * c;
+
+// c
+// This program performs a sample payroll calculation.
+
+// 2.14
+public void displayAdjacentNumbers(){
+        System.out.println("\n1 2  3 4");
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.print(" 3");
+        System.out.print(" 4\n");
+        System.out.printf("%s %s  %s %s","1","2","3","4");
+}
+
+// 2.15
+    public void doSomeArithmeticWithUserInput(){
+        System.out.println("Please enter two whole numbers of your choice below");
+        System.out.println
+    }
+
+//public void x%(){
+//
+//    }
+
+
+    public static void main(String... args){
+        ExerciseTwoPrograms supply = new ExerciseTwoPrograms();
+        supply.displayEnterAnInteger();
+        supply.displayAdjacentNumbers();
+
+
+
+    }
+}
