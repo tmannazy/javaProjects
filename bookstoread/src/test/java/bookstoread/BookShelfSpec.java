1c72a72884e62f4b6c5409e324bebaae09164c28
tmannazy <e.anene@hotmail.com>
2022-04-23 02:50:17 +0100

    Practice on JUnit5 test using gradle

---
+++ bookstoread/src/test/java/bookstoread/BookShelfSpec.java
@@ -0,0 +1,29 @@
+package bookstoread;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/*public class BookShelfSpec {
+    @Test
+    public void shelfEmptyWhenNoBookAdded() throws Exception {
+        BookShelf shelf = new BookShelf();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");;
+
+    }
+
+}*/
+
+class BookShelfSpec {
+    private BookShelfSpec() {
+        BookShelf shelf = new BookShelf();
+    }
+    @Test
+    void shelfEmptyWhenNoBookAdded() throws Exception {
+
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");;
+
+    }
+}
