d89891952d39c1a600868cf97e1d9e77d1903d5d
tmannazy <e.anene@hotmail.com>
2022-05-02 02:25:22 +0100

    Display triangle patterns using a nested loop to the console

--- Salah_Snacks/src/TrianglePrinter.java
+++ Salah_Snacks/src/TrianglePrinter.java
@@ -8,8 +8,6 @@ public class TrianglePrinter {
         }
 
         System.out.println();
-        System.out.println();
-        System.out.println();
 
         for (int i = 10; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
