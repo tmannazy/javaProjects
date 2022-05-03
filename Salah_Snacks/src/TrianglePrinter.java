4df5d3d06b31bacc5cc8d9b015bc6c6bcd35e239
tmannazy <e.anene@hotmail.com>
2022-05-03 19:34:44 +0100

    Update codebase to print patterns of triangles with '*'

--- Salah_Snacks/src/TrianglePrinter.java
+++ Salah_Snacks/src/TrianglePrinter.java
@@ -1,7 +1,19 @@
 public class TrianglePrinter {
     public static void main(String... args) {
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = i; j <= 10; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -9,31 +21,28 @@ public class TrianglePrinter {
 
         System.out.println();
 
-        for (int i = 10; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = i; k <= 10; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        for (int i = 10; i >= 0; i--) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            for (int k = 10; k >= i; k--) {
-                System.out.print(" ");
-            }
-        }
+        System.out.println();
 
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = i; j <= 10; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
-            for (int k = 10; k >= i; k--) {
-                System.out.print(" ");
-            }
         }
     }
 }
