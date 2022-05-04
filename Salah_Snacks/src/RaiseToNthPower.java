4e0bc3e95400903f2ba1b9faad30dec21f1eb3d9
tmannazy <e.anene@hotmail.com>
2022-05-04 14:02:00 +0100

    Refactor code, rename file and update class codebase

--- Salah_Snacks/src/RaiseToNthPower.java
+++ Salah_Snacks/src/RaiseToNthPower.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RaiseToPowerN {
+public class RaiseToNthPower {
     public static void main(String... args) {
         System.out.print("Enter the base number: ");
         Scanner scanner = new Scanner(System.in);
