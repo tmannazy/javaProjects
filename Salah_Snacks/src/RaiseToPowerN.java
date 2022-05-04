1c95f2ea9370f399fc1d6f20197dbfa4c9e5d9c7
tmannazy <e.anene@hotmail.com>
2022-05-04 03:41:18 +0100

    Find the n-th power of any given number from user

---
+++ Salah_Snacks/src/RaiseToPowerN.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RaiseToPowerN {
+    public static void main(String... args) {
+        System.out.print("Enter the base number: ");
+        Scanner scanner = new Scanner(System.in);
+        int base = scanner.nextInt();
+        System.out.print("Enter the n-th power: ");
+        int power = scanner.nextInt();
+        int result = 1;
+        int savedPower = power;
+
+        while (power != 0) {
+            result *= base;
+            power--;
+        }
+        System.out.printf("The value to the power %d of %d is %d.",savedPower, base, result);
+    }
+}
