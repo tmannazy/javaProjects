b193a25155767cfdf84f01ff0f91eb8d50fdd270
tmannazy <e.anene@hotmail.com>
2022-05-18 12:46:57 +0100

    Remove redundant and unused codes

--- Chapter_Four_Dietel/src/TabularOutput.java
+++ Chapter_Four_Dietel/src/TabularOutput.java
@@ -2,10 +2,10 @@ public class TabularOutput {
     public static void main(String... args) {
         System.out.println("N\t\tN2\t\tN3\t\tN4");
         System.out.println();
-        for (int i = 1, n = 1; i <= 5; i++, n++) {
+        for (int i = 1; i <= 5; i++) {
             int p = 1;
-            for (int k = 1, s = 1; k <= 4; k++, s++) {
-                if (i == 1) System.out.print(n * n + "\t\t");
+            for (int k = 1; k <= 4; k++) {
+                if (i == 1) System.out.print(i * i + "\t\t");
                 if (i >= 2) {
                     p *= i;
                     System.out.print(p + "\t\t");
