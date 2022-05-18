e08024febe4a7192c0cd8bc033c96c941a753da7
tmannazy <e.anene@hotmail.com>
2022-05-18 12:43:47 +0100

    Fix logic to mulptiply the base integer with iteration power

--- Chapter_Four_Dietel/src/TabularOutput.java
+++ Chapter_Four_Dietel/src/TabularOutput.java
@@ -1,19 +1,17 @@
-import java.awt.font.TextAttribute;
-
 public class TabularOutput {
     public static void main(String... args) {
+        System.out.println("N\t\tN2\t\tN3\t\tN4");
+        System.out.println();
         for (int i = 1, n = 1; i <= 5; i++, n++) {
-            for (int j = 1; j <= 4; j++) {
-                if (i == 1) System.out.print("N\t\t");
+            int p = 1;
+            for (int k = 1, s = 1; k <= 4; k++, s++) {
+                if (i == 1) System.out.print(n * n + "\t\t");
+                if (i >= 2) {
+                    p *= i;
+                    System.out.print(p + "\t\t");
+                }
             }
-
-            for (int k = 1; k <= 4; k++) {
-                int p = n;
-//                if (i == 1) {System.out.print(n * n);
-//                }
-                if (i >= 2) System.out.print(p++ * i + "\t\t");
-            }
-                System.out.println();
+            System.out.println();
         }
     }
 }
