61cf73da05f8bfc587a10d29615673d465092f17
tmannazy <e.anene@hotmail.com>
2022-05-30 18:19:15 +0100

    Separated files to clean up main branch

--- Chapter_Four_Dietel/src/DanglingElse.java
+++
@@ -1,9 +0,0 @@
-public class DanglingElse {
-    public static void main(String... args) {
-        int x = 10, y = 9;
-        if (x > 5) {
-            if (y > 5) System.out.println("x and y are > 5");
-        } else
-            System.out.println("x is <= 5");
-    }
-}
--- Chapter_Four_Dietel/src/SalesCommissionCalculatorTest.java
+++
@@ -1,9 +0,0 @@
-public class SalesCommissionCalculatorTest {
-    public static void main(String... args) {
-        SalesCommissionCalculator sales = new SalesCommissionCalculator(1, 239.99);
-        sales.setItemSold(2, 129.75);
-        sales.setItemSold(3, 99.95);
-        sales.setItemSold(4, 350.89);
-        System.out.format("Your commission for last week sales is %.2f", sales.commission());
-    }
-}
